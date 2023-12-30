368a700eb83a3b8fb05d11d7f92675b159deffe6
kimobrian <kimocoding@gmail.com>
2023-12-30 23:14:49 +0300

    fix sign in and user creation

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,44 +1,45 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
-import { User } from '@models/user'
-
+import { User } from "@models/user";
 
 const handler = NextAuth({
-  providers: [GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  })],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email})
-    session.user.id = sessionUser._id.toString()
-    return session
-  },
-  async signIn({ profile}) {
-    try {
-      // serverless -> lambda
-      await connectToDB();
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda
+        await connectToDB();
 
-      const userExists = await User.findOne({
-        email: profile.email
-      })
-
-      // If not, create a new user
-      if(!userExists) {
-        await User.create({
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").trim().toLowercase(),
-          image: profile.picture
-        })
+        });
+
+        // If not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile?.name?.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
-    }
-  }
-})
+    },
+  },
+});
 
-
-export { handler as GET, handler as POST}
-
+export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,24 +6,28 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { sendError } from "next/dist/server/api-utils";
 
 export const Nav = () => {
-  const isUserLoggedIn = true;
+
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setupProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
+
+    setupProviders()
   }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image
+        <Image 
           src="/assets/images/logo.svg"
           alt="Logo"
           width={30}
@@ -34,17 +38,17 @@ export const Nav = () => {
       </Link>
       {/* Desktop nav */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create_prompt" className="black_btn">
-              Create Post
+              Create Prompt
             </Link>
             <button type="button" className="outline_btn" onClick={signOut}>
               Sign Out
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width="37"
                 height="37"
                 className="rounded-full"
@@ -71,10 +75,10 @@ export const Nav = () => {
 
       {/** Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               width="37"
               height="37"
               className="rounded-full"
