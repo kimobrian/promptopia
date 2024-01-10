6c1a4e14e6faca81fc8263d40becb2f3397dbe4a
kimobrian <kimocoding@gmail.com>
2024-01-10 14:23:42 +0300

    implement post copy

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,12 @@ import { usePathname, useRouter } from 'next/navigation'
 
 export const PromptCard = ({ post, handleTagClick }) => {
   const [copied, setCopied] = useState('')
+
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(''), 3000)
+  }
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -27,7 +33,7 @@ export const PromptCard = ({ post, handleTagClick }) => {
             </p>
           </div>
         </div>
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
@@ -40,6 +46,13 @@ export const PromptCard = ({ post, handleTagClick }) => {
           />
         </div>
       </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   )
 }
